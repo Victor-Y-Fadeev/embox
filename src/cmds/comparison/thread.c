9a0d486168449e4f3f78139f9cfb12a537b71df2
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2019-05-27 00:31:36 +0300

    Fix thread

--- src/cmds/comparison/thread.c
+++ src/cmds/comparison/thread.c
@@ -7,64 +7,63 @@
  */
 
 #include <stdio.h>
-#include <kernel/task.h>
-#include <unistd.h>
 #include <pthread.h>
-#include <util/err.h>
-#include <time.h>
 #include "environment.h"
 
 #define ITER 100
 
-int t[3]= {0,0,0};
+int period[3]= { 0, 0, 0 };
 
-int curent[3] = {0,0,0};
+int current[3] = { 0, 0, 0 };
 int var[ITER][3];
 
-pthread_t th1, th2, th3;
+pthread_t th, th2, th3;
 
 
-static void * Task(void *nuber){
-    int n = (int)nuber;
-    int t1, t2, cur_time;
+static void *task(void *arg) {
+	int n = (int) arg;
+	int first, second, cur_time;
 
-    while(curent[n] < ITER) {
-        t1 = t[(n + 1) % 3];
-        t2 = t[(n + 2) % 3];
-        cur_time = clock();
+	while (current[n] < ITER) {
+		first = period[(n + 1) % 3];
+		second = period[(n + 2) % 3];
+		cur_time = clock();
 
-        if ((t1 != 0) && (t2 != 0))
-        {
-            if (t1 > t2)
-            {
-                var[curent[n]][n] = cur_time - t2;
-            }
-            else
-            {
-                var[curent[n]][n] = cur_time - t1;
-            }
-            curent[n]++;
-        }
-        t[n] = clock();
-    }
-   
-   thread_exit(NULL); 
+		if (first != 0 && second != 0) {
+			if (first < second) {
+				var[current[n]][n] = cur_time - first;
+			} else {
+				var[current[n]][n] = cur_time - second;
+			}
+			current[n]++;
+		}
+		period[n] = clock();
+	}
+
+ 	return NULL; 
 }
 
 int main(int argc, char **argv) { 
-    int varFull[ITER * 3];
+	int var_full[ITER * 3];
 
-    pthread_create(&th1, NULL, Task, (void *) 0);
-    pthread_create(&th2, NULL, Task, (void *) 1);
-    pthread_create(&th3, NULL, Task, (void *) 2);
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
 
-    for (int i = 0; i < ITER; i++) {
-        for (int j = 0; j < 3; j++) {
-            varFull[i * 3 + j] = var[i][j];
-        }
+	pthread_create(&th, &attr, task, (void *) 0);
+	pthread_create(&th2, &attr, task, (void *) 1);
+	pthread_create(&th3, &attr, task, (void *) 2);
+
+	pthread_join(th, NULL);
+	pthread_join(th2, NULL);
+	pthread_join(th3, NULL);
+
+	for (int i = 0; i < ITER; i++) {
+		for (int j = 0; j < 3; j++) {
+			var_full[i * 3 + j] = var[i][j];
+		}
 	}
 
-    output("Process", varFull, ITER * 3);
+	output("Thread", var_full, ITER * 3);
 
-    return 0;
+	return 0;
 }
