15ac1d2aa192bfcf106cb694d2ec1898dc9f098e
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2019-05-27 01:17:30 +0300

    Fix IRQ

--- src/cmds/comparison/irq.c
+++ src/cmds/comparison/irq.c
@@ -3,66 +3,55 @@
  * @brief
  *
  * @date 26.05.19
- * @author Victor Y. Fadeev
+ * @author Egor Anikin
  */
 
 #include <stdio.h>
-#include <kernel/task.h>
-#include <unistd.h>
 #include <pthread.h>
-#include <util/err.h>
-#include <time.h>
 #include <kernel/irq.h>
 #include "environment.h"
 
 #define ITER 1000000
-#define TEST_IRQ_NR 10
+#define IRQ_NR 10
 
 pthread_t th;
 
-static irq_return_t test_isr(unsigned int irq_nr, void *dev_id) {
+
+static irq_return_t isr(unsigned int irq_nr, void *dev_id) {
 	//irqctrl_clear(irq_nr);
 	return IRQ_HANDLED;
 }
 
 static void *task(void *arg) {
-	int cur_time = 0;
-	int prev_time = 0;
-	int ave = 0.0;
+	int cur_time, prev_time;
+	double average;
 
 	prev_time = clock();
-	for(int i=0; i < ITER; i++)
-	{
-		irqctrl_force(TEST_IRQ_NR); 
+	for (int i = 0; i < ITER; i++) {
+		irqctrl_force(IRQ_NR); 
 	}
 	cur_time = clock();
 
-	ave = cur_time - prev_time;
+	average = cur_time - prev_time;
 
 	prev_time = clock();
-	for(int i=0; i < ITER; i++)
-	{
-		
-	}
+	for (int i = 0; i < ITER; i++);
 	cur_time = clock();
 
-	ave = (ave - cur_time + prev_time);
-     
-    printf("ave = %d\n", ave);
-	//single("irq", ave);
+	average = (average - cur_time + prev_time) / ITER;
+	single("IRQ", average);
 
 	return NULL;
 }
 
 int main(int argc, char **argv) {
+	irq_attach(IRQ_NR, isr, 0, (void *) '0', "irq");
 
-	irq_attach(TEST_IRQ_NR, test_isr, 0, (void *) '0', "test_irq");
-
-
-	pthread_create(&th, NULL, task, NULL);
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_create(&th, &attr, task, NULL);
 
 	pthread_join(th, NULL);
-
-	irq_detach(TEST_IRQ_NR, (void *) '0');
+	irq_detach(IRQ_NR, (void *) '0');
 	return 0;
 }
