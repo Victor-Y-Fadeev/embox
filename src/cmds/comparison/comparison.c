e439e29b64e6e410bc579f36310b338aea0806bb
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2019-05-27 06:50:36 +0300

    Fix semaphore

--- src/cmds/comparison/comparison.c
+++ src/cmds/comparison/comparison.c
@@ -2,90 +2,14 @@
  * @file
  * @brief
  *
- * @date 17.05.19
- * @author Egor Anikin
+ * @date 16.05.19
+ * @author Victor Y. Fadeev
  */
 
 #include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <kernel/sched/sync/mutex.h>
-#include <kernel/thread/sync/mutex.h>
 #include "environment.h"
 
-#define ITER 1000
-#define TASK 2
-#define MD(x) do {\
-x;\
-}while(0);
 
-
-struct mutex M;
-pthread_t th1, th2, th3;
-
-int curent = 0;
-int curent2 = 0;
-int chek[ITER*TASK];
-int chek2[ITER*TASK];
-double ave[TASK];
-//int ave = -1;
-
-
-static inline void *task(void *arg) {
-    int n = (int)arg; 
-
-    int cur_time = 0;
-	int prev_time = 0;
-
-	prev_time = clock();
-	for(int i = 0; i < ITER; i++)
-	{
-		mutex_lock(&M);
-            chek[curent] += 1;
-            curent++;
-        mutex_unlock(&M);
-	}
-	cur_time = clock();
-
-	ave[n] = cur_time - prev_time;
-
-	prev_time = clock();
-	for(int i = 0; i < ITER; i++)
-	{
-		chek2[curent2] += 1;
-        curent2++;
-	}
-	cur_time = clock();
-
-	ave[n] = (ave[n] - cur_time + prev_time)/ITER; 
-   return NULL; 
+int main(int argc, char **argv) {
+	return 0;
 }
-
-int main(int argc, char **argv) { 
-
-    for (int i = 0; i < ITER*2; i++) {
-        chek[i] = -1;
-    }
-
-	pthread_attr_t attr;
-    pthread_attr_init(&attr);
-
-    mutex_init(&M);
-    pthread_create(&th1, &attr, task, (void *)0);
-    pthread_create(&th2, &attr, task, (void *)1);
-
-    pthread_join(th1, NULL);
-    pthread_join(th2, NULL);
-
-    
-    for (int i = 0; i < ITER*TASK; i+=10) {
-        if (chek[i] != 0) {
-            error("Semaphore"); 
-        }
-    }
-    ave[0] =  (ave[0] + ave[1])/2;
-    single("Semaphore",ave[0]);
-
-    return 0;
-}
-
--- src/cmds/comparison/semaphore.c
+++ src/cmds/comparison/semaphore.c
@@ -2,7 +2,7 @@
  * @file
  * @brief
  *
- * @date 17.05.19
+ * @date 27.05.19
  * @author Egor Anikin
  */
 
@@ -15,77 +15,69 @@
 
 #define ITER 1000
 #define TASK 2
-#define MD(x) do {\
-x;\
-}while(0);
 
-
-struct mutex M;
 pthread_t th1, th2, th3;
+struct mutex m;
 
-int curent = 0;
-int curent2 = 0;
-int chek[ITER*TASK];
-int chek2[ITER*TASK];
-double ave[TASK];
-//int ave = -1;
+int current[2] = { 0, 0 };
+int check[2][ITER * TASK];
+double average[TASK];
 
 
-static inline void *task(void *arg) {
-    int n = (int)arg; 
-
-    int cur_time = 0;
-	int prev_time = 0;
+static void *task(void *arg) {
+	int n = (int) arg;
+	int cur_time, prev_time;
 
 	prev_time = clock();
-	for(int i = 0; i < ITER; i++)
-	{
-		mutex_lock(&M);
-            chek[curent] += 1;
-            curent++;
-        mutex_unlock(&M);
+	for (int i = 0; i < ITER; i++) {
+		mutex_lock(&m);
+		check[0][current[0]]++;
+		current[0]++;
+		mutex_unlock(&m);
 	}
 	cur_time = clock();
 
-	ave[n] = cur_time - prev_time;
+	average[n] = cur_time - prev_time;
 
 	prev_time = clock();
-	for(int i = 0; i < ITER; i++)
-	{
-		chek2[curent2] += 1;
-        curent2++;
+	for (int i = 0; i < ITER; i++) {
+		check[1][current[1]]++;
+		current[1]++;
 	}
 	cur_time = clock();
 
-	ave[n] = (ave[n] - cur_time + prev_time)/ITER; 
-   return NULL; 
+	average[n] = (average[n] - cur_time + prev_time) / ITER;
+
+	return NULL; 
 }
 
-int main(int argc, char **argv) { 
+int main(int argc, char **argv) {
+	for (int i = 0; i < ITER * TASK; i++) {
+		check[0][i] = -1;
+	}
 
-    for (int i = 0; i < ITER*2; i++) {
-        chek[i] = -1;
-    }
+	mutex_init(&m);
 
 	pthread_attr_t attr;
-    pthread_attr_init(&attr);
+	pthread_attr_init(&attr);
 
-    mutex_init(&M);
-    pthread_create(&th1, &attr, task, (void *)0);
-    pthread_create(&th2, &attr, task, (void *)1);
+	pthread_create(&th1, &attr, task, (void *) 0);
+	pthread_create(&th2, &attr, task, (void *) 1);
 
-    pthread_join(th1, NULL);
-    pthread_join(th2, NULL);
+	pthread_join(th1, NULL);
+	pthread_join(th2, NULL);
 
-    
-    for (int i = 0; i < ITER*TASK; i+=10) {
-        if (chek[i] != 0) {
-            error("Semaphore"); 
-        }
-    }
-    ave[0] =  (ave[0] + ave[1])/2;
-    single("Semaphore",ave[0]);
+	for (int i = 0; i < ITER * TASK; i++) {
+		if (check[0][i] != 0) {
+			error("Semaphore fail"); 
+		}
+	}
 
-    return 0;
+	double result = 0;
+	for (int i = 0; i < TASK; i++) {
+		result += average[i];
+	}
+
+	single("Semaphore", result / TASK);
+	return 0;
 }
-
